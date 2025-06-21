cc9b94e85346dabd7934bf589a986ec08e4426e9
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-21 11:13:28 +0200

    Trashegimia

---
+++ task2Exception/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //Kur nuk jane statike metodat, per ti thirrur duhet te krijojme objekte
+
+        BookRepository bookRepository = new BookRepository();
+
+        //shtimin e librave
+        bookRepository.addBook(new Book(1,"titulli1","author1",1999));
+    }
+}
