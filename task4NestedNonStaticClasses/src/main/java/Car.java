9b143d5e7acb70c8228b4f9d81974781644ea04c
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-21 12:47:00 +0200

    Interfaces

---
+++ task4NestedNonStaticClasses/src/main/java/Car.java
@@ -0,0 +1,29 @@
+public class Car {
+    /*Car class
+Create the  Car  class which will store information about a car make and type. It
+should contain  getter  and  setter  methods.
+Engine class
+Implement the  Engine  class which will be a nested non-static class under the
+Car  class. This class should contain the eld: engine type and  setEngine
+method which will set a type based on the car type. If the car type is  economy ,
+then the engine type should be set to  diesel . If the car type is  luxury , then the
+engine type should be dened as  electric . Otherwise, the engine type should
+be dened as  petrol*/
+
+
+    private String make;
+    private String make;
+
+    public Car(String make, String make1) {
+        this.make = make;
+        this.make = make1;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+}
