63e76cae1c94b4623aa5450dc46d1ab2b7d6e6c3
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-21 10:42:59 +0200

    Trashegimia

--- task1Exception/src/main/java/Division.java
+++ task1Exception/src/main/java/Division.java
@@ -1,5 +1,7 @@
 import java.sql.SQLOutput;
-
+/*Create the  divide  method which has to divide the two numbers that are the
+attributes of the method. In case the second parameter of the method is 0, a
+non-default exception should be thrown:  CannotDivideBy0Exception */
 public class Division {
 
     public static double Divide(double numerator, double demoninator)throws CannotDivideBy0Exception{
