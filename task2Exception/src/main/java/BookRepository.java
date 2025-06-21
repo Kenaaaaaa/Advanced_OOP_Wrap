242ea69368eab4391fba8367419265c0b10f1811
kenaaaaaaa <kenatirana@yahoo.com>
2025-06-21 11:02:46 +0200

    Trashegimia

--- task2Exception/src/main/java/BookRepository.java
+++ task2Exception/src/main/java/BookRepository.java
@@ -1,2 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+/*BookRepository class
+Create the  BookRepository  class, which will be responsible for:
+adding  Book  objects
+removing  Book  objects
+searching for objects of the  Book  type with the indicated name
+searching for objects of the  Book  type with the indicated id
+removing objects of the  Book  type based on the provided  id
+Book class
+The  Book  class should include the following elds:
+id
+title
+author
+year of release
+NoBookFoundException
+In case of lack of searched results an exception should be thrown. This
+exception should accept the  String  parameter object with information about
+which elements could not be found*/
 public class BookRepository {
+
+    //Krijojme nje liste te librave
+    private List<Book> books;
+    //konstruktori metode e vecante qe ka emrin e klases, dhe perdoret per krijimin  e objekteve
+    public BookRepository(){
+        this.books=new ArrayList<>();
+    }
+
+    //metoda per te shtuar nje liber ne liste
+    public void addBook(Book book){
+        books.add(book);
+    }
+    //metoda per te hequr librin sipas id
+    public void removeBookById(int id)throws NoBookFoundException{
+        Book bookFound=findBookById(id);
+        books.remove(bookFound);
+    }
+
+    //metoda per te gjetur librin sipas id
+    public Book findBookById(int id)throws NoBookFoundException{
+        for(Book book:books){
+            if(book.getId() ==id){
+                return book;
+            }
+        }
+            throw new NoBookFoundException("Ky liber me id "+id+" nuk ekziston.");
+
+
+    }
+
 }
